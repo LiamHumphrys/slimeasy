8b0400820e5e1b08ec92c424d58b4d21a1708bf1
LiamHumphrys <liamhumphrys@hotmail.com>
2025-03-18 16:21:07 +1030

    Delete tracker-fixed.js

--- tracker-fixed.js
+++
@@ -1,7 +0,0 @@
-/**
- * SlimEasy - Tracker Fixed Module
- * This file redirects to the main tracker.js implementation.
- * It exists to maintain compatibility but all functionality is now in tracker.js.
- */
-
-console.log("tracker-fixed.js is deprecated. Using tracker.js instead.");
